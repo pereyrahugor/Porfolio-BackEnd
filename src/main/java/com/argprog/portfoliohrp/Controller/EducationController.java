ceb72aac33a7dcc36c5417415a4922fdb93c5681
Pereyra Hugo <62813900+pereyrahugor@users.noreply.github.com>
2023-02-07 15:50:26 -0300

    Update EducationController.java
    Prueba Access-Control-Allow-Origin: para pernitir accesos

--- src/main/java/com/argprog/portfoliohrp/Controller/EducationController.java
+++ src/main/java/com/argprog/portfoliohrp/Controller/EducationController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/Educacion")
-@CrossOrigin (origins = "https://porfolio-frontend-argprog-hrp.web.app")
+@CrossOrigin (origins = "Access-Control-Allow-Origin: https://porfolio-frontend-argprog-hrp.web.app")
 public class EducationController {
     @Autowired
     ImpEducationService impEducationService;
